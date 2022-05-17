2571ff0b69e3c0b27795eda289e2cfda92280d46
Artem <artemer981333@gmail.com>
2022-05-17 18:10:31 +0700

    Connection added

---
+++ connectiontoserver.cpp
@@ -0,0 +1,6 @@
+#include "connectiontoserver.h"
+
+ConnectionToServer::ConnectionToServer()
+{
+
+}
---
+++ connectiontoserver.h
@@ -0,0 +1,12 @@
+#ifndef CONNECTIONTOSERVER_H
+#define CONNECTIONTOSERVER_H
+
+#include <QObject>
+
+class ConnectionToServer
+{
+public:
+	ConnectionToServer();
+};
+
+#endif // CONNECTIONTOSERVER_H
